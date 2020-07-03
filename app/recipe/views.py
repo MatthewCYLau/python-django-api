fd195491623de332ed0044c2eb227a932381e180
Matthew Lau <matthewlau@matthews-mbp.mynet>
2020-07-03 15:14:45 +0100

    add Ingredient

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 from recipe import serializers
 
@@ -49,11 +49,17 @@ class TagViewSet(viewsets.GenericViewSet,
         """Create a new tag"""
         serializer.save(user=self.request.user)
 
-# class IngredientViewSet(BaseRecipeAttrViewSet):
-#     """Manage ingredients in the database"""
-#     queryset = Ingredient.objects.all()
-#     serializer_class = serializers.IngredientSerializer
 
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin):
+    """Manage ingredients in the database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-name')
 
 # class RecipeViewSet(viewsets.ModelViewSet):
 #     """Manage recipes in the database"""
